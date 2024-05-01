96b8e7115140ef90984d86a13395f48f8155ef5e
brayan gomez <brayangomezo2005@gmail.com>
2024-04-30 21:03:34 -0500

    avance de los martes

---
+++ src/components/pages/auth/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Login.css";
+import { iniDatabase } from "../../config/firebaseConfig";
+import { collection } from "firebase/firestore";
+
+const Login = () => {
+
+  function getUsuarios(){
+    let resultado = collection(iniDatabase,'usuarios');
+    console.log(resultado);
+  }
+  getUsuarios()
+
+  console.log(iniDatabase)
+  return (
+    <div className="login-page">
+      <div className="form">
+        <form className="register-form">
+          <input type="text" placeholder="name" />
+          <input type="password" placeholder="password" />
+          <input type="text" placeholder="email address" />
+          <button>create</button>
+          <p className="message">
+            Already registered? <a href="#">Sign In</a>
+          </p>
+        </form>
+        <form className="login-form">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button type="button">login</button>
+          <p className="message">
+            Not registered? <a href="#">Create an account</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
